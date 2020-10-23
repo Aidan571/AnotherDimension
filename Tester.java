9a04365dabf2005ba5b85e279f1b91f36396babe
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-23 13:31:06 -0400

    Added arrToString and deepArrToString methods.

--- Tester.java
+++ Tester.java
@@ -1,4 +1,25 @@
 public class Tester{
+  public static String arrToString(int[] arr){
+  String result = "{";
+  if(arr.length == 0){
+      return "{}";
+    }
+  for(int i = 0; i < arr.length; i++){
+    if(i == arr.length-1){
+      result = result + arr[i] + "}";
+    }
+    else
+    result = result + arr[i] + ", ";
+  }
+  return result;
+  }
+  public static String arrayDeepToString(int[][] arr){
+  String result = "{";
+  for(int i = 0; i < arr.length; i++){
+      result = result + arrToString(arr[i]) + ", ";
+  }
+  return result + "}";
+  }
   public static void main(String[]args){
     int[] a = {1,2,3,4,5};
     System.out.println(ArrayOps.sum(a));
