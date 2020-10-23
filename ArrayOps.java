6b814bfd5dad60eddc8571205b78f9588f5d6d44
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-23 13:12:46 -0400

    Created sum method.

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,12 @@
 public class ArrayOps{
-  
+  public static int sum(int[] arr){
+    if(arr.length == 0){
+      return 0;
+    }
+    int result = 0;
+    for(int i = 0; i < arr.length; i++){
+      result += arr[i];
+    }
+    return result;
+  }
 }
