49ed57a0257eca066cd0eb0f9d59ea681b2546a4
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-10-24 15:58:34 -0400

    Tested sumCols method.

--- Tester.java
+++ Tester.java
@@ -24,9 +24,11 @@ public class Tester{
     int[] a = {1,2,3,4,5};
     System.out.println(ArrayOps.sum(a));
     System.out.println(ArrayOps.largest(a));
-    int[][] b ={{1,2,3,4}, {10,30,20}, {5,10}, {5}};
+    int[][] b = {{1,2,3,4}, {10,30,20}, {5,10}, {5}};
     System.out.println(arrToString(ArrayOps.sumRows(b)));
     System.out.println(arrToString(ArrayOps.largestInRows(b)));
     System.out.println(ArrayOps.sum(b));
+    int[][] c = {{1,2,3,4,5}, {2,3,4,5,1}, {3,4,5,1,2}, {4,5,1,2,3}, {5,1,2,3,4}};
+    System.out.println(arrToString(ArrayOps.sumCols(c)));
   }
 }
